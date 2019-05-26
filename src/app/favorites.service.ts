60acb68119b9f96d62f8c8ff13fee249c8a3e091
Spencer K-W <keashlywarters@gmail.com>
2019-05-26 16:14:50 -0400

    created favorites service

---
+++ src/app/favorites.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoritesService {
+
+  favorites: any[]; //favorite recipes
+
+  constructor() { }
+
+  addFavorite(recipe: any): any[] {
+    this.favorites.push(recipe);
+    return this.favorites;
+  }
+
+  removeFavorite(index: number): any[] {
+    this.favorites.splice(index, 1);
+    return this.favorites;
+  }
+}
