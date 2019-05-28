267749e3c3bc326859a67990120276aba2aa7f61
Spencer K-W <keashlywarters@gmail.com>
2019-05-28 09:56:59 -0400

    favorites can be added and removed, and icon changes to show

--- src/app/recipe-card/recipe-card.component.ts
+++ src/app/recipe-card/recipe-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FavoritesService } from '../favorites.service';
 
 @Component({
   selector: 'recipe-card',
@@ -9,13 +10,21 @@ export class RecipeCardComponent implements OnInit {
 
   @Input() recipe: any;
 
-  constructor() { }
+  constructor(private favorites: FavoritesService) { }
 
   ngOnInit() {
   }
 
   goToRecipeDetail() {
 
+  }
+
+  toggleFavorite() {
+    if (this.favorites.favoritesContains(this.recipe)) {
+      this.favorites.removeFavorite(this.recipe);
+    } else {
+      this.favorites.addFavorite(this.recipe);
+    }
   }
 
 }
